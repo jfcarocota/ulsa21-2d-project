dcdd05b867780f0dda5f96bb7fcde6a6ced54fb6
jfcarocota <jfcarocota@gmail.com>
2021-01-21 09:10:48 -0700

    publush

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake");
+    } 
+
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+}
